4a8285afa52b783d19e18b2ac5dfb3e001a3a966
Josh Keife <jkeife@gmail.com>
2020-07-22 22:17:44 -0600

    set area id and case note

--- app.py
+++ app.py
@@ -8,15 +8,16 @@ import os
 import re
 import requests
 
+DATA_URL = os.getenv('DATA_URL', 'https://coronavirus-dashboard.utah.gov')
+
 app = Flask(__name__)
 app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 
 
 def get_cases():
     data = []
-    url = "https://coronavirus-dashboard.utah.gov"
     try:
-        r = requests.get(url)
+        r = requests.get(DATA_URL)
         r.raise_for_status()
         soup = BeautifulSoup(r.content, 'html.parser')
         draper = soup.find(string=re.compile("Draper"))
@@ -25,20 +26,25 @@ def get_cases():
             i = i.replace('<strong>Name: </strong>', '')
             i = i.replace('<br><b>Case Count:</b> ', ',')
             i = i.replace('<br><b>Case Rate / 100,000:</b> ', ',')
+            area_id = re.search(r"^\d.*-\d*\.\d|^\d.*-\d*", i)[0]
             i = re.sub(r"^\d.*-\d*\.\d |^\d.*-\d* ", "", i)
             i = i.split(',')
 
+            if 'Data Note' in i[2]:
+                note = i[2].split('<br>')[1]
+                note = re.sub(r"<b>Data Note:</b> ", "", note)
+                case_rate = float(re.sub(r"<br><b>Data Note:</b>.*", "", i[2]))
+            else:
+                case_rate = float(i[2])
+                note = ""
+
             rate = {
                 'area': i[0],
-                'case_count': i[1],
-                'case_rate': i[2]
+                'area_id': area_id,
+                'case_count': int(i[1]),
+                'case_rate': case_rate,
+                'case_note': note
             }
-
-            if 'Data Note' in rate['case_rate']:
-                note = rate['case_rate'].split('<br>')[1]
-                note = re.sub(r"<b>Data Note:</b> ", "", note)
-                rate['case_note'] = note
-                rate['case_rate'] = re.sub(r"<br><b>Data Note:</b>.*", "", rate['case_rate'])
 
             data.append(rate)
         return True, data
